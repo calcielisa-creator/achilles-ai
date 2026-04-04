c5ab28592118e4c513d4194248a1328d5d023702
calcielisa-creator <calcielisa@gmail.com>
2026-04-04 17:18:18 +0200

    environment variables, reading API key, getting a test response

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
+import os
+from dotenv import load_dotenv
+from google import genai
+
+
 def main():
     print("Hello from achilles-ai!")
 
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    if api_key == None:
+        raise RuntimeError("Environment variable NOT FOUND")
 
+    client = genai.Client(api_key=api_key)
+
+    contents= "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+    model = "gemini-2.5-flash"
+    response = client.models.generate_content(model = model, contents = contents)
+    print(response.text)
 if __name__ == "__main__":
     main()

1326c17b682085320fba0680a73c48b57abc6a44
calcielisa-creator <calcielisa@gmail.com>
2026-04-08 10:34:50 +0200

    update: prompt as a command-line argument

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import os
 from dotenv import load_dotenv
 from google import genai
-
+import argparse
 
 def main():
     print("Hello from achilles-ai!")
+
+    parser = argparse.ArgumentParser(description="achilles-ai")
+    parser.add_argument("user_prompt", type=str, help="User prompt")
+    args = parser.parse_args()
 
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
@@ -13,13 +17,15 @@ def main():
 
     client = genai.Client(api_key=api_key)
 
-    contents= "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
     model = "gemini-2.5-flash"
+    contents= args.user_prompt
+    
     response = client.models.generate_content(model = model, contents = contents)
+    
     if not response.usage_metadata:
         raise RuntimeError("FAILED API request")
     
-    print("Prompt tokens: ",response.usage_metadata.prompt_token_count)
+    print("Prompt tokens: ", response.usage_metadata.prompt_token_count)
     print("Response tokens: ", response.usage_metadata.candidates_token_count)
     print("Response:")
     print(response.text)

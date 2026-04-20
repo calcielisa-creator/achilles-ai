d7fcd8457c88017c98e022dcf66e7eb0ec2c523b
calcielisa-creator <calcielisa@gmail.com>
2026-04-20 10:23:03 +0200

    adding a function that can get the contents of a file

---
+++ functions/get_file_content.py
@@ -0,0 +1,22 @@
+import os
+from config import MAX_CHARS
+
+def get_file_content(working_directory, file_path):
+    try:
+        absolute_path = os.path.abspath(working_directory)
+        target_path = os.path.normpath(os.path.join(absolute_path, file_path))
+        valid_target_dir = os.path.commonpath([absolute_path, target_path]) == absolute_path
+        if valid_target_dir == False:
+            return f'Error: Cannot list "{file_path}" as it is outside the permitted working directory'
+        if os.path.isfile(target_path) == False:
+            return f'Error: File not found or is not a regular file: "{file_path}"'
+    
+        with open(target_path, "r") as f:
+            file_content_string = f.read(MAX_CHARS)
+        # After reading the first MAX_CHARS...
+            if f.read(1):
+                file_content_string += f'[...File "{file_path}" truncated at {MAX_CHARS} characters]'
+
+        return file_content_string
+    except:
+        return f"Error: errors raised by standard library functions"

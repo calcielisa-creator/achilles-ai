b466de838421eb45bb09d71c60f175d601dbd309
calcielisa-creator <calcielisa@gmail.com>
2026-04-21 18:38:44 +0200

    setting up a basic system prompt

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ import argparse
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
+from prompts import system_prompt
 
 def main():
-    print("Hello from achilles-ai!")
 
     parser = argparse.ArgumentParser(description="achilles-ai")
     parser.add_argument("user_prompt", type=str, help="User prompt")
@@ -20,7 +20,7 @@ def main():
     client = genai.Client(api_key=api_key)
     messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
 
-    response = client.models.generate_content(model = "gemini-2.5-flash", contents = messages)
+    response = client.models.generate_content(model = "gemini-2.5-flash", contents = messages, config=types.GenerateContentConfig(system_instruction=system_prompt, temperature=0),)
     
     if not response.usage_metadata:
         raise RuntimeError("FAILED API request")
